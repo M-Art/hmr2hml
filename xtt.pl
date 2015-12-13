9a275865c42baec909a51c62200f2d021a158787
M-Art <m-art91@tlen.pl>
2015-12-13 22:11:50 +0100

    Parse table rules' conditions

--- xtt.pl
+++ xtt.pl
@@ -7,8 +7,9 @@ table :-
     xschm TableName : _ ==> _,
     fwriteln '<table id="tab_~w" name="~w">' ~ TableName ~ TableName,
     --> table_schema(TableName),
+    --> table_rules(TableName),
     fwriteln '</table>',
-    !.
+    fail.
 table.
 
 table_schema(TableName) :-
@@ -28,4 +29,51 @@ table_schema_precondition([Precondition|Preconditions]) :-
 
 table_schema_conclusion([Conclusion|Conclusions]) :-
     fwriteln '<attref ref="attr_~w"/>' ~ Conclusion.
+
+table_rules(TableName) :-
+    xrule TableName/Num : _ ==> _,
+    fwriteln '<rule id="rul_~w_~w">' ~ TableName ~ Num,
+    --> table_rule(TableName, Num),
+    fwriteln '</rule>',
+    fail.
+table_rules(_).
+
+table_rule(TableName, Num) :-
+    xrule TableName/Num : Conditions ==> Decisions : Link,
+    table_rule_conditions(Conditions),
+    !.
+table_rule(TableName, Num) :-
+    xrule TableName/Num : Conditions ==> Decisions,
+    table_rule_conditions(Conditions),
+    !.
+
+table_rule_conditions(Conditions) :-
+    fwriteln '<condition>',
+    --> table_rule_condition(Conditions),
+    fwriteln '</condition>'.
+
+table_rule_condition([]).
+table_rule_condition([Condition|Conditions]) :-
+    table_rule_condition_relation(Condition),
+    table_rule_condition(Conditions).
+
+table_rule_condition_relation(Condition) :-
+    Condition =.. [Relation, Name, Value],
+    fwriteln '<relation name="~w">' ~ Relation,
+    --> fwriteln '<attref ref="attr_~w"/>' ~ Name,
+    --> fwriteln '<set>',
+    --> --> table_rule_condition_relation_set(Value),
+    --> fwriteln '</set>',
+    fwriteln '</relation>'.
+
+table_rule_condition_relation_set([]).
+table_rule_condition_relation_set([L to H]) :-
+    fwriteln '<value from="~w" to="~w"/>' ~ L ~ H,
+    !.
+table_rule_condition_relation_set([Value|Values]) :-
+    table_rule_condition_relation_set(Value),
+    table_rule_condition_relation_set(Values),
+    !.
+table_rule_condition_relation_set(Value) :-
+    fwriteln '<value is="~w"/>' ~ Value.
 
